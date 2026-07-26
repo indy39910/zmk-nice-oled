0ba0d7506678705ac48eee8109f4e4dabe6cfa75
cioe <168082388+indy39910@users.noreply.github.com>
2026-07-26 17:39:11 +0800

    my logo added

---
+++ boards/shields/nice_oled/assets/my_logo.h
@@ -0,0 +1,11 @@
+#ifndef _MY_LOGO_H
+#define _MY_LOGO_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "lvgl.h"
+LV_IMG_DECLARE(my_logo);
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+#endif /* _MY_LOGO_H */
